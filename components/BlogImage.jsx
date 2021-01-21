6ad91ba2169ae420bf3ff5bbcb0eec73d3c28ee3
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-22 02:23:19 +0530

    Changed img to Image (next/image)

--- components/BlogImage.jsx
+++ components/BlogImage.jsx
@@ -2,7 +2,13 @@ import Image from "next/image";
 export default function BlogImage({ imageUrl, className }) {
   return (
     <div className={className}>
-      <img src={imageUrl} alt="Blog Image" />
+      <Image
+        src={imageUrl}
+        alt="Blog Image"
+        height={500}
+        width={900}
+        layout="responsive"
+      />
     </div>
   );
 }
