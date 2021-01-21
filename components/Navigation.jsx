69cf913ef226f4e768c9b2164398b45a5f0111c1
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-22 02:06:51 +0530

    Navigation functional Component Created

---
+++ components/Navigation.jsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/navigation.module.css";
+
+export default function Navigation({ links }) {
+  return (
+    <div className={styles["nav-bar"]}>
+      <div className={styles["logo"]}>
+        <Image src={links.logo.url} alt="" height="50px" width="50px" />
+      </div>
+      <div className={styles["nav-links"]}>
+        {links.navigation_links.map((link) => {
+          return (
+            <div key={link.display_name}>
+              <Link href={link.href}>
+                <a>{link.display_name}</a>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
