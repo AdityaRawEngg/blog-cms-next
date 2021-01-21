ff27c478110575eca35f6f03fb35f589b0b9465b
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-22 01:49:44 +0530

    Added a custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import styles from "../styles/404.module.css";
+import Image from "next/image";
+
+export default function FourOhFour() {
+  return (
+    <>
+      <Image
+        src="https://optinmonster.com/wp-content/uploads/2018/06/11-brilliant-404-pages-to-turn-lost-visitors-into-loyal-customers-2.png"
+        alt="404 Page"
+        layout="fill"
+      />
+    </>
+  );
+}
