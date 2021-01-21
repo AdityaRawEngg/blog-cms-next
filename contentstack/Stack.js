914db9ef5a9314931affa3d4dde776cad6299f98
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-22 02:59:48 +0530

    console.log removed

--- contentstack/Stack.js
+++ contentstack/Stack.js
@@ -4,5 +4,4 @@ const Stack = Contentstack.Stack(
   process.env.DELIVERY_TOKEN,
   process.env.ENVIRONMENT
 );
-console.log("Stack", Stack);
 export default Stack;
--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-module.exports = {
-  // env: {
-  //   API_KEY: "Your API Key",
-  //   DELIVERY_TOKEN: "Your Delivery Token",
-  //   ENVIRONMENT: "Your Environment",
-  // },
-  images: {
-    domains: ["images.contentstack.io", "optinmonster.com"],
-  },
+module.exports = (phase, { defaultConfig }) => {
+  return {
+    images: {
+      domains: ["images.contentstack.io"],
+    },
+  };
 };
