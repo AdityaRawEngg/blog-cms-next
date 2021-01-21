4822214093ec608653ec2caa4d539ad5f99a08c9
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-22 02:22:36 +0530

    Changed img to Image (next/image)

--- components/Aside.jsx
+++ components/Aside.jsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 import Paragraph from "../components/Paragraph";
 
 export default function Aside({ id, title, image, className, renderBlog }) {
   return (
     <div onClick={renderBlog} className={className} id={id}>
-      <img src={image} alt="Blog Image" />
+      <Image src={image} alt="Blog Image" height={300} width="" />
       <Paragraph>{title}</Paragraph>
     </div>
   );
