a509e5845bbb6b6e46faa4c4e79690d07150df19
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-22 02:41:54 +0530

    Console.log added

--- contentstack/Stack.js
+++ contentstack/Stack.js
@@ -4,4 +4,5 @@ const Stack = Contentstack.Stack(
   process.env.DELIVERY_TOKEN,
   process.env.ENVIRONMENT
 );
+console.log("Stack", Stack);
 export default Stack;
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  // env: {
+  //   API_KEY: "Your API Key",
+  //   DELIVERY_TOKEN: "Your Delivery Token",
+  //   ENVIRONMENT: "Your Environment",
+  // },
   images: {
     domains: ["images.contentstack.io", "optinmonster.com"],
   },
