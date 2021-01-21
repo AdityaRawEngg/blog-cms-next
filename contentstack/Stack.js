549aa81eaf0eb932e1ec50c6e843beb85c6d431b
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-22 03:13:21 +0530

    Changed Stack config

--- contentstack/Stack.js
+++ contentstack/Stack.js
@@ -1,8 +1,8 @@
 import Contentstack from "contentstack";
-const Stack = Contentstack.Stack(
-  process.env.API_KEY,
-  process.env.DELIVERY_TOKEN,
-  process.env.ENVIRONMENT,
-  "us"
-);
+const Stack = Contentstack.Stack({
+  api_key: process.env.API_KEY,
+  delivery_token: process.env.DELIVERY_TOKEN,
+  environment: process.env.ENVIRONMENT,
+  region: "us",
+});
 export default Stack;
