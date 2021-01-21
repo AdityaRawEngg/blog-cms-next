b5ee215250ec4f45468078308380d11044ae9c36
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-22 03:54:01 +0530

    Send onclick function as a prop

--- components/Aside.jsx
+++ components/Aside.jsx
@@ -3,9 +3,9 @@ import Paragraph from "../components/Paragraph";
 
 export default function Aside({ id, title, image, className, renderBlog }) {
   return (
-    <div onClick={renderBlog} className={className} id={id}>
+    <div className={className} id={id}>
       <Image src={image} alt="Blog Image" height={300} width="" />
-      <Paragraph>{title}</Paragraph>
+      <Paragraph renderBlog={renderBlog}>{title}</Paragraph>
     </div>
   );
 }
