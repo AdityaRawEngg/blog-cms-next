d40345c87a247ab8bffb73dd75dcd7bf17af7a95
AdityaRawEngg <aditya.shukla@raweng.com>
2021-01-22 11:01:00 +0530

    Added Loading when no props are loaded

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -6,18 +6,27 @@ export default function Navigation({ links }) {
   return (
     <div className={styles["nav-bar"]}>
       <div className={styles["logo"]}>
-        <Image src={links.logo.url} alt="" height="50px" width="50px" />
+        {links.logo.url ? (
+          <>
+            <Image src={links.logo.url} alt="" height="50px" width="50px" />
+          </>
+        ) : (
+          <>
+            <p>Loading</p>
+          </>
+        )}
       </div>
       <div className={styles["nav-links"]}>
-        {links.navigation_links.map((link) => {
-          return (
-            <div key={link.display_name}>
-              <Link href={link.href}>
-                <a>{link.display_name}</a>
-              </Link>
-            </div>
-          );
-        })}
+        {links.navigation_links &&
+          links.navigation_links.map((link) => {
+            return (
+              <div key={link.display_name}>
+                <Link href={link.href}>
+                  <a>{link.display_name}</a>
+                </Link>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
